13d9ad553ff3bc6815170dfd7cf9ce98f419a96e
mincheoly <mincheoly@gmail.com>
2017-03-30 21:54:24 -0700

    Modules for RD pipeline

---
+++ sherlock_scripts/apply_dr.py
@@ -0,0 +1,68 @@
+################################################################################
+# Authors: Min Cheol Kim, Christian Choe
+
+# Description: Applies a dimensionality reduction on a given dataset. 
+
+# Usage: python isomap_clustering.py -method <method> -n_neighbors <n_neighbors> -n_components <n_components> -dataset <dataset label>
+# method is the method to use. Currently: KernelPCA, Isomap, and SpectralEmbedding
+# n_neighbors is a number of neighbors to use
+# n_components is components in the ISOMAP space
+# dataset is which dataset to use
+
+# Example: python apply_dr.py -method isomap -n_neighbors 100 -n_components 100 -dataset apo_calmodulin -sample_rate 0.1
+
+# Output: 1 .dat file containing the transformed coordinates.
+# These files are saved with a *_<n_neighbors>_<n_components>_<sample_rate>.dat naming scheme.
+
+################################################################################
+
+# imports
+import numpy as np
+import argparse as ap
+from sklearn import manifold
+from sklearn import decomposition
+from sklearn.externals import joblib
+
+# additional imports
+import tempfile
+import os
+os.chdir(tempfile.mkdtemp())
+
+# Set up ArgumentParser
+parser = ap.ArgumentParser(description='Fitting model script.')
+parser.add_argument('-method', action="store", dest='method')
+parser.add_argument('-n_components', action="store", dest='n_components', type=int)
+parser.add_argument('-n_neighbors', action="store", dest='n_neighbors', type=int)
+parser.add_argument('-dataset', action='store', dest='which_dataset')
+parser.add_argument('-sample_rate', action='store', dest='sample_rate', type=float)
+args = parser.parse_args()
+
+# Assign argument variables
+method = args.method
+n_neighbors = args.n_neighbors
+n_components = args.n_components
+which_dataset = args.which_dataset
+sample_rate = args.sample_rate
+
+# Combine n_neighbors and n_components to produce an ID
+ID = str(n_neighbors) + '_' + str(n_components) + '_' + str(sample_rate)
+
+# Load appropriate X matrix
+X = np.load('/scratch/users/mincheol/' + which_dataset + '/raw_XYZ.dat')
+
+# Load the appropriate model 
+model = joblib.load('/scratch/users/mincheol/' + which_dataset + '/models/' + method + '_model_' + ID + '.pkl')
+
+# Transform X matrix in batches
+X_rd = np.array((0,n_components))
+num_batches = 100
+batch_size = X.shape[0]/num_batches # size of each batch
+for batch in range(num_batches+1):
+	start_idx = batch * batch_size
+	end_idx = (batch + 1)*batch_size if (batch + 1)*batch_size < X.shape[0] else X.shape[0]
+	X_rd = np.concatenate([X_rd, model.transform(X[start_idx:end_idx, :])])
+
+# Saved X in reduced dimension
+X_rd.dump('/scratch/users/mincheol/' + which_dataset + '/reduced_dimension/X_'+ method +'_' + ID + '.dat')
+print('Coordinates saved in reduced dimension')
+
---
+++ sherlock_scripts/fit_model.py
@@ -0,0 +1,76 @@
+################################################################################
+# Authors: Min Cheol Kim, Christian Choe
+
+# Description: Fits a dimensionality reduction model and saves the model itself using picke.
+
+# Usage: python fit_model.py -method <method> -n_neighbors <n_neighbors> -n_components <n_components> -dataset <dataset label> -sample_rate <sampling rate>
+# method is the method to use. Currently: KernelPCA, Isomap, and SpectralEmbedding
+# n_neighbors is a number of neighbors to use
+# n_components is components in the ISOMAP space
+# dataset is which dataset to use
+# sample_rate is the subsampling rate
+
+# Example: python fit_model.py -method isomap -n_neighbors 40 -n_components 10 -dataset fspeptide -sample_rate 0.1
+
+# Output: 1 pkl file containing the model.
+# These files are saved with a *_<n_neighbors>_<n_components>_<sample_rate>.dat naming scheme.
+
+################################################################################
+
+# get data
+import numpy as np
+import argparse as ap
+from sklearn import manifold
+from sklearn import decomposition
+from sklearn.externals import joblib
+
+# additional imports
+import tempfile
+import os
+os.chdir(tempfile.mkdtemp())
+
+# Set up ArgumentParser
+parser = ap.ArgumentParser(description='Fitting model script.')
+parser.add_argument('-method', action="store", dest='method')
+parser.add_argument('-n_components', action="store", dest='n_components', type=int)
+parser.add_argument('-n_neighbors', action="store", dest='n_neighbors', type=int)
+parser.add_argument('-dataset', action='store', dest='which_dataset')
+parser.add_argument('-sample_rate', action='store', dest='sample_rate', type=float)
+args = parser.parse_args()
+
+# Assign argument variables
+method = args.method
+n_neighbors = args.n_neighbors
+n_components = args.n_components
+which_dataset = args.which_dataset
+sample_rate = args.sample_rate
+
+# Combine n_neighbors and n_components to produce an ID
+ID = str(n_neighbors) + '_' + str(n_components) + '_' + str(sample_rate)
+
+# Load appropriate X matrix
+X = np.load('/scratch/users/mincheol/' + which_dataset + '/raw_XYZ.dat')
+
+# Get dimensions of the X matrix
+num_frames = X.shape[0]
+num_features = X.shape[1]
+
+# Subsample
+desired_num_frames = int(round(sample_rate*num_frames))
+indices = [i for i in range(num_frames)]
+np.random.shuffle(indices)
+indices = indices[:desired_num_frames]
+X_sampled = X[indices,:]
+
+#apply dimensionality reduction, fit the model using sample data and transform all other frames as well
+if method == 'kernelPCA':
+	model = decomposition.kernelPCA(n_components=n_components, kernel='rbf', n_jobs=-1)
+elif method == 'isomap':
+	model = manifold.Isomap(n_neighbors=n_neighbors, n_components=n_components, n_jobs=-1)
+else: # method is spectral embedding
+	model = manifold.SpectralEmbedding(n_neighbors=n_neighbors, n_components=n_components, n_jobs=-1)
+
+# Fit and save the model
+X_iso_sampled = model.fit(X_sampled)
+joblib.dump(model, '/scratch/users/mincheol/' + which_dataset + '/models/' + method + '_model_' + ID + '.pkl')
+print('Model Saved')
